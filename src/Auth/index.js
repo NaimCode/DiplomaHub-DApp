e024d31d0067e402e41d66c1ecfbe2d6ea17c104
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-03-29 14:41:58 +0000

    redesign login page

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -4,10 +4,13 @@ import {
   FormControlLabel,
   Radio,
   Button,
+  Box,
+  Card,
 } from "@mui/material";
 import { EmailOutlined, LockOutlined } from "@mui/icons-material";
 import * as animationData from "/public/animations/login.json";
 import Lottie from "react-lottie";
+import Logo from "../Components/Logo";
 const Auth = () => {
   const defaultOptions = {
     loop: true,
@@ -19,53 +22,65 @@ const Auth = () => {
   };
   return (
     <div className="h-screen w-screen flex">
-      <div className="h-full grow flex flex-col justify-center items-center">
-        <div className="p-3 w-2/3">
-          <p>Bienvenue sur</p>
-          <h1>Certification Enligne</h1>
-          <TextField
-            fullWidth
-            label="Email"
-            InputProps={{
-              endAdornment: <EmailOutlined />,
-            }}
-          />
-          <TextField
-            type={"password"}
-            className="my-4"
-            fullWidth
-            label="Mot de passe"
-            InputProps={{
-              endAdornment: <LockOutlined />,
-            }}
-          />
-          <RadioGroup
-            className="flex flex-row gap-3 justify-between"
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="etudiant"
-          >
-            <FormControlLabel
-              value="etudiant"
-              control={<Radio />}
-              label="Etudiant"
-            />
-            <FormControlLabel
-              value="member"
-              control={<Radio />}
-              label="Membre"
-            />
-          </RadioGroup>
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            className="font-corps_1 my-2 py-3"
-          >
-            Connexion
-          </Button>
+      <div className="grow flex flex-col items-center">
+        <div className="h-full flex flex-col p-4 max-w-md">
+          <div className="flex items-center h-16 ">
+            <Logo />
+          </div>
+          <Box className="grow flex flex-col justify-center">
+            <Card className="px-4 py-10">
+              <p className="text-accent">Content de vous revoir</p>
+              <h1 className="titre1">Se connecter à votre espace</h1>
+              <hr className="my-5" />
+
+              <div className="py-5">
+                <TextField
+                  fullWidth
+                  label="Email"
+                  InputProps={{
+                    endAdornment: <EmailOutlined />,
+                  }}
+                />
+                <TextField
+                  type={"password"}
+                  className="my-4"
+                  fullWidth
+                  label="Mot de passe"
+                  InputProps={{
+                    endAdornment: <LockOutlined />,
+                  }}
+                />
+                <RadioGroup
+                  className="flex flex-row gap-3 justify-between"
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="etudiant"
+                >
+                  <FormControlLabel
+                    value="etudiant"
+                    control={<Radio />}
+                    label="Etudiant"
+                  />
+                  <FormControlLabel
+                    value="member"
+                    control={<Radio />}
+                    label="Membre"
+                  />
+                </RadioGroup>
+              </div>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className="font-corps_1 mt-2 py-3"
+              >
+                Connexion
+              </Button>
+            </Card>
+          </Box>
+          <div className="grow"></div>
         </div>
       </div>
-      <div className="h-full w-0 lg:w-1/2 bg-primaire-normal border-l-2 border-gray-500">
+      <div className="h-full w-0 md:w-1/2 bg-primaire-normal border-l-2 border-gray-500">
         <Lottie options={defaultOptions} />
       </div>
     </div>
