80d34be0ffe633f0c6e6421b1e3d5d47873fc337
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-05-07 18:33:24 +0000

    ready to first deploy

--- src/Data/serveur.js
+++ src/Data/serveur.js
@@ -1,4 +1,5 @@
-export const SERVER_URL = process.env.SERVER_URL || "http://localhost:4000";
+export const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
 export const IPFS_NODE =
   process.env.IPFS_NODE || "https://ipfs.infura.io/ipfs/";
 export const CERTIFICATION_SMART_CONTRACT_ADDRESS =
